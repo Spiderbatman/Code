ab8866cad975844210e31ac03d086e23b0c0dd8d
spiderbatman <gigi.pataraia@gmail.com>
2019-03-16 12:37:47 +0400

    fist commit

---
+++ src/Problem.java
@@ -0,0 +1,20 @@
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Random;
+
+public class Problem {
+	
+	public static void main(String[] args) {
+		System.out.println("DWADAW");
+	}
+	
+
+}
